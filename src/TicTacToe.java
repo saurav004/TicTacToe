3b22b7c295b0665e4647bba5152e0781587ef36a
saurav004 <logtosaurav@gmail.com>
2020-11-03 18:41:00 +0530

    Added:user gets to place X or O in board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	static char[] board = new char[10];
+	static char player = ' ';
 
 	public static void main(String[] args) {
-		char player = ' ';
+
 		char computer = ' ';
 		System.out.println("Welcome to TicTacToe");
 		populateBoard();
@@ -15,13 +16,29 @@ public class TicTacToe {
 			computer = 'X';
 		}
 		System.out.println(player + " is player " + computer + " is computer");
-
 		showBoard();
+		makeaMove();
+	}
+
+	private static void makeaMove() {
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter a slot to place " + player);
+			int slot = scan.nextInt();
+			if (board[slot] == ' ') {
+				board[slot] = player;
+				break;
+			} else {
+				System.out.println("Invalid choice,Enter again");
+			}
+		}
+		showBoard();
+
 	}
 
 	private static void showBoard() {
 		for (int i = 1; i < board.length; i = i + 3) {
-			System.out.println("| " + board[i] + " | " + board[i + 1] + " | " + board[i + 2] + " |");
+			System.out.println("|" + board[i] + " |" + board[i + 1] + " |" + board[i + 2] + " |");
 		}
 	}
 
