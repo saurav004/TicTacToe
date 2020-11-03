1a756e3f04cf57949ab6780e151a567784b18d55
saurav004 <logtosaurav@gmail.com>
2020-11-03 15:50:43 +0530

    Modified:Correct Indentation is applied

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,16 +1,17 @@
 
-
 public class TicTacToe {
 	static char[] board;
+
 	public static void main(String[] args) {
-		board=new char[10];
-	System.out.println("Welcome to TicTacToe");
-	
-	populateBoard();
-}
+		board = new char[10];
+		System.out.println("Welcome to TicTacToe");
+
+		populateBoard();
+	}
+
 	static void populateBoard() {
 		for (int i = 1; i < 10; i++) {
 			board[i] = ' ';
 		}
-}
+	}
 }
