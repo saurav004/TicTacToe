b1ff628257363d9622ff281e456e545b7908bf1b
saurav004 <logtosaurav@gmail.com>
2020-11-03 15:41:18 +0530

    Added board is populated with single space at each position

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,16 @@
 
 
 public class TicTacToe {
-	public static void main() {
+	static char[] board;
+	public static void main(String[] args) {
+		board=new char[10];
 	System.out.println("Welcome to TicTacToe");
+	
+	populateBoard();
+}
+	static void populateBoard() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
 }
 }
